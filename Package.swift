9ca03f6d4924b40aef0a0f7e2550fb293de41b93
take8 <37664118+take8@users.noreply.github.com>
2022-06-18 16:25:53 +0900

    Fix: Xcodeで開いた時にエラーになっていたので修正

--- Package.swift
+++ Package.swift
@@ -3,18 +3,19 @@
 import PackageDescription
 
 let package = Package(
-  name: "github_search_repository",
-  targets: [
-    .target(
-      name: "GitHubSearch"
-    )
-    .testTarget(
-      name: "GitHubSearchTests",
-      dependencies: ["GitHubSearch"]
-    )
-    .target(
-      name: "github_search_repository",
-      dependencies: ["GitHubSearch"]
-    )
-  ]
+    name: "github_search_repository",
+    targets: [
+        .target(
+            name: "GitHubSearch"),
+        .testTarget(
+            name: "GitHubSearchTests",
+            dependencies: [
+                "GitHubSearch",
+            ]),
+        .target(
+            name: "github_search_repository",
+            dependencies: [
+                "GitHubSearch",
+            ]),
+    ]
 )
