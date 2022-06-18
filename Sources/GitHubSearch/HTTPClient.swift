8c1825077e420dc5a6c755c4b194fa71335be2da
take8 <37664118+take8@users.noreply.github.com>
2022-06-19 01:07:21 +0900

    Add: APIクライアント(Stubを別ファイル化)

--- Sources/GitHubSearch/HTTPClient.swift
+++ Sources/GitHubSearch/HTTPClient.swift
@@ -27,20 +27,3 @@ extension URLSession : HTTPClient {
         task.resume()
     }
 }
-
-class StubHTTPClient : HTTPClient {
-    var result: Result<(Data, HTTPURLResponse), Error> = .success((
-        Data(),
-        HTTPURLResponse(
-            url: URL(string: "https://example.com")!,
-            statusCode: 200,
-            httpVersion: nil,
-            headerFields: nil)!
-    ))
-
-    func sendRequest(_ urlRequest: URLRequest, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [unowned self] in
-            completion(self.result)
-        }
-    }
-}
