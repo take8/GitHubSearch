5a08e386a75ed892db7540262e9ef1b8224d558d
take8 <37664118+take8@users.noreply.github.com>
2022-06-18 22:38:43 +0900

    Add: APIリクエストのモデル化

---
+++ Sources/GitHubSearch/GitHubRequest.swift
@@ -0,0 +1,21 @@
+//
+//  GitHubRequest.swift
+//  
+//
+//  Created by Takeya Shimizu on 2022/06/18.
+//
+
+import Foundation
+
+public protocol GitHubRequest {
+    var baseURL: URL { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var queryItems: [URLQueryItem] { get }
+}
+
+public extension GitHubRequest {
+    var baseURL: URL {
+        return URL(string: "https://api.github.com")!
+    }
+}
